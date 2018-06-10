540d006f8ac03b0194f44b1c3a1c229d5e89c70e
Geisa Nepomuceno <geisa.victoria@gmail.com>
2018-06-10 20:58:11 -0300

    adding client class builder

---
+++ src/model/Cliente.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Cliente {
+	private int codigo;
+	private String nome; 
+	private String endereco; 
+	private String cidade; 
+	private String bairro; 
+	private int cep; 
+	private String rg;
+	private String cpf;
+	private String dataNasc; 
+	private int telefone; 
+	private String email; 
+	private int celular;
+	private char sexo; 
+	
+	
+}
