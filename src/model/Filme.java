dba2d67319beffce179cf913fb5451c42150341f
Saito <viniciusaitosilva@gmail.com>
2018-07-05 12:44:31 -0300

    Att Filme/FilmeDAO

--- src/model/Filme.java
+++ src/model/Filme.java
@@ -1,79 +1,120 @@
 package model;
 
 public class Filme {
-	private int codigoFilme;
+	private int codfilme;
 	private String nome; 
 	private String dataLancamento; 
 	private String diretor; 
 	private String bairro; 
-	private int	 tempo; 
+	private String	 tempo; 
 	private String genero;
 	private String classificacao;
 	private String descricao;
-	private int codigoFunc;
+	private int codfuncionario;
 	
 	public void mostrar(){
-        System.out.println("["+codigoFilme+","+nome+","+dataLancamento+","+diretor+","+bairro+","+tempo+","+genero+","+classificacao+","+descricao+","+codigoFunc+"]");
+        System.out.println("["+codfilme+","+nome+","+dataLancamento+","+diretor+","+bairro+","+tempo+","+genero+","+classificacao+","+descricao+","+codfuncionario+"]");
     }
 	
-	public int getCodigoFilme() {
-		return codigoFilme;
+	public Filme() {
+		
 	}
-	public void setCodigoFilme(int codigoFilme) {
-		this.codigoFilme = codigoFilme;
+	
+
+	public Filme(int codfilme, String nome, String dataLancamento, String diretor, String bairro, String tempo,
+			String genero, String classificacao, String descricao, int codfuncionario) {
+		super();
+		this.codfilme = codfilme;
+		this.nome = nome;
+		this.dataLancamento = dataLancamento;
+		this.diretor = diretor;
+		this.bairro = bairro;
+		this.tempo = tempo;
+		this.genero = genero;
+		this.classificacao = classificacao;
+		this.descricao = descricao;
+		this.codfuncionario = codfuncionario;
 	}
+
+	public int getCodfilme() {
+		return codfilme;
+	}
+
+	public void setCodfilme(int codfilme) {
+		this.codfilme = codfilme;
+	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getDataLancamento() {
 		return dataLancamento;
 	}
+
 	public void setDataLancamento(String dataLancamento) {
 		this.dataLancamento = dataLancamento;
 	}
+
 	public String getDiretor() {
 		return diretor;
 	}
+
 	public void setDiretor(String diretor) {
 		this.diretor = diretor;
 	}
+
 	public String getBairro() {
 		return bairro;
 	}
+
 	public void setBairro(String bairro) {
 		this.bairro = bairro;
 	}
-	public int getTempo() {
+
+	public String getTempo() {
 		return tempo;
 	}
-	public void setTempo(int tempo) {
+
+	public void setTempo(String tempo) {
 		this.tempo = tempo;
 	}
+
 	public String getGenero() {
 		return genero;
 	}
+
 	public void setGenero(String genero) {
 		this.genero = genero;
 	}
+
 	public String getClassificacao() {
 		return classificacao;
 	}
+
 	public void setClassificacao(String classificacao) {
 		this.classificacao = classificacao;
 	}
+
 	public String getDescricao() {
 		return descricao;
 	}
+
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
-	public int getCodigofunc() {
-		return codigoFunc;
+
+	public int getCodfuncionario() {
+		return codfuncionario;
 	}
-	public void setCodigofunc(int codigofunc) {
-		this.codigoFunc = codigofunc;
+
+	public void setCodfuncionario(int codfuncionario) {
+		this.codfuncionario = codfuncionario;
 	}
+	
+	
 }
--- src/model/FilmeDAO.java
+++ src/model/FilmeDAO.java
@@ -1,5 +1,187 @@
 package model;
 
-public class FilmeDAO {
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+import integracaobd.BDLocal;
+
+public class FilmeDAO {
+	
+	private String men, sql; 
+	private BDLocal bd;
+	
+	public FilmeDAO() {
+		bd = new BDLocal();
+	}
+	
+	public String salvar(Filme c) {//METODO INSERIR OU ATUALIZAR
+		sql = "INSERT\r\n" + 
+				"	INTO filme(id_filme,nome,datalancamento,diretor,tempo,genero,classificacao,descricao,id_funcionario)\r\n" + 
+				"VALUES\r\n" + 
+				"	(?,?,?,?,?,?,?,?,?)";
+		
+		try {
+			bd.getConnection();
+			bd.st = bd.con.prepareStatement(sql);
+                        bd.st.setInt(1, c.getCodfilme());
+                        bd.st.setString(2, c.getNome());
+                        bd.st.setString(3, c.getDataLancamento());
+                        bd.st.setString(4, c.getDiretor());
+                        bd.st.setString(5, c.getTempo());
+                        bd.st.setString(6, c.getGenero());
+                        bd.st.setString(7, c.getClassificacao());
+                        bd.st.setString(8, c.getDescricao());
+                        bd.st.setInt(9, c.getCodfuncionario());
+                        
+                        
+
+			bd.st.executeUpdate();
+			
+			men = "Filme cadastrado com sucesso!";
+		}
+		catch (SQLException erro) {
+				int pos = erro.toString().indexOf("PRIMARY KEY");
+				if(pos>=0) { 
+					sql = "UPDATE\r\n" + 
+							"	filme\r\n" + 
+							"SET\r\n" + 
+							"	(id_filme=?,nome=?,datalancamento=?,diretor=?,tempo=?,genero=?,classificacao=?,descricao=?,id_funcionario=?)\r\n" + 
+							"WHERE\r\n" + 
+							"	id_filme=?";
+					try {
+						
+						bd.st.setInt(1, c.getCodfilme());
+                        bd.st.setString(2, c.getNome());
+                        bd.st.setString(3, c.getDataLancamento());
+                        bd.st.setString(4, c.getDiretor());
+                        bd.st.setString(5, c.getTempo());
+                        bd.st.setString(6, c.getGenero());
+                        bd.st.setString(7, c.getClassificacao());
+                        bd.st.setString(8, c.getDescricao());
+                        bd.st.setInt(9, c.getCodfuncionario());
+                        bd.st.setInt(10, c.getCodfilme());
+						bd.st.executeUpdate();
+						men = "Filme alterado com sucesso!";
+					}
+		catch(SQLException e) {
+                    men = "Falha na alteração!";
+		}
+		finally {
+                    bd.close();
+		}	
+                                }
+                                
+                }
+                
+                return men;
+        }
+	
+public String excluir(int codigo) { //METODO DELETE
+		
+		sql = "delete from filme where id_filme = ?";
+		
+		try {
+			
+			bd.getConnection(); 
+			bd.con.prepareStatement(sql); 
+			bd.st.setInt(1, codigo);
+			
+			int r = bd.st.executeUpdate(); 
+			
+			if (r==1) {
+				
+				men = "Excluido com sucesso!";		
+				
+			} else {
+				
+				men = "Filme nao encontrado!";
+			}
+			
+		} catch (Exception e) {
+			
+			men = "Falha! " + e.toString();
+		}
+		finally {
+			bd.close();
+		}
+		
+		return men;		
+	}
+
+public Filme localizar(int codigo) {//METODO SELECT
+	
+	sql = "select * from filme where id_filme = ?";
+	
+	Filme c = new Filme();
+	
+	try {
+		
+		bd.getConnection();
+		bd.con.prepareStatement(sql);
+		bd.st.setInt(1, codigo);
+		bd.rs = bd.st.executeQuery();
+		
+		if(bd.rs.next()) {		
+			c.setCodfilme(bd.rs.getInt("Código filme"));
+			c.setNome(bd.rs.getString("nome"));
+			c.setDataLancamento(bd.rs.getString("Data de lançamento"));
+			c.setDiretor(bd.rs.getString("Diretor"));
+			c.setTempo(bd.rs.getString("Tempo de duração"));
+			c.setGenero(bd.rs.getString("Gênero"));
+			c.setClassificacao(bd.rs.getString("Classificação"));
+			c.setDescricao(bd.rs.getString("Descrição"));
+			c.setCodfuncionario(bd.rs.getInt("Código funcionário"));
+		}
+		
+		else {
+			c = null;
+		}
+	} catch (Exception e) {
+		c = null;
+	}
+	
+	finally {
+		bd.close();
+	}
+	
+	return c;
+}
+
+public List<Filme> getFilmes() {//METODO LISTAR TODOS OS CLIENTES (LISTA)
+	
+	List<Filme> lista = new ArrayList<Filme>(); // Criar uma lista de produtos
+	
+	// ler o banco do comeï¿½o ao fim
+	sql = "select * from filme";
+	
+	try {
+		
+		bd.getConnection(); // conexao com o banco
+		bd.st = bd.con.prepareStatement(sql);
+		bd.rs = bd.st.executeQuery(); // rs um objeto em memï¿½ria que representa uma tabela com os dados do select, a primeira linha ï¿½ o header da coluna
+		
+		while (bd.rs.next()) {			
+			Filme c = new Filme();
+			
+			c.setCodfilme(bd.rs.getInt("Código filme"));
+			c.setNome(bd.rs.getString("nome"));
+			c.setDataLancamento(bd.rs.getString("Data de lançamento"));
+			c.setDiretor(bd.rs.getString("Diretor"));
+			c.setTempo(bd.rs.getString("Tempo de duração"));
+			c.setGenero(bd.rs.getString("Gênero"));
+			c.setClassificacao(bd.rs.getString("Classificação"));
+			c.setDescricao(bd.rs.getString("Descrição"));
+			c.setCodfuncionario(bd.rs.getInt("Código funcionário"));
+			
+			lista.add(c); // adicionando o produto na lista
+		}
+
+	} 
+	catch (Exception e) {	lista = null;} 
+	finally {bd.close();}
+	
+	return lista;
+}
+	
 }
